1d45f885f84738db155f7afb9343d3f8274296b9
Matias Ruiz <ruizmat@gmail.com>
2019-09-01 20:32:05 +0200

    checking relative urls

--- check.sh
+++ check.sh
@@ -8,6 +8,7 @@ urls="";
 lenghtHref=0;
 paginasEncontradas=0;
 paginasNoEncontradas=0;
+enlacesRelativos=0;
 while read -n1 c;
 do
   cadena=$cadena$c
@@ -15,7 +16,7 @@ done < $1
 tamanyo=${#cadena}
 while [ $cont -lt $tamanyo ];
 do
-  if [ "${cadena:cont:4}" == "href" ]
+  if [ "${cadena:cont:6}" == "href=\"" ]
   then
     contUrl=$((cont+6));
     while [ "${cadena:contUrl:1}" != "\"" ]
@@ -26,10 +27,16 @@ do
     if [ $lenghtHref != 0 ]
     then 
       url=${cadena:$((cont+6)):$lenghtHref};
-      respuesta=$(curl -Is $url | head -n 1);
-      urls=$urls$url"\t"$respuesta"\n";
-      echo -e $url
-      echo -e $respuesta
+      if [ "${url:0:1}" == "/" ]
+      then
+        echo -e "\e[31mAtencion! Enlace relativo\e[0m "$url
+        ((enlacesRelativos++))
+      else 
+        respuesta=$(curl -Is $url | head -n 1);
+        urls=$urls$url"\t"$respuesta"\n";
+        echo -e $url
+        echo -e $respuesta
+      fi
     else
       echo -e "\e[31mEnlace "$((contEnlaces+1))" vacio!\e[0m"
       ((enlacesVacios++))
@@ -41,7 +48,7 @@ done
 echo
 echo -e "\e[1m"$contEnlaces" enlaces analizados\e[0m"
 paginasEncontradas=$(echo -e $urls | grep -v "HTTP...4.." | wc -l);
-echo -e "\e[1m"$paginasEncontradas" paginas encontradas\e[0m"
+echo -e "\e[1m"$((paginasEncontradas-1))" paginas encontradas\e[0m"
 echo -e $urls | grep -v "HTTP...4.."
 echo
 paginasNoEncontradas=$(echo -e $urls | grep "HTTP...4.." | wc -l);
@@ -50,3 +57,4 @@ echo -e $urls | grep "HTTP...4.."
 echo
 echo -e "\e[31m"$enlacesVacios" enlaces vacios\e[0m"
 echo
+echo -e "\e[31m"$enlacesRelativos " enlaces relativos\e[0m"
